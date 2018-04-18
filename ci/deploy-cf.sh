567ab201b1650c555e2c4eb343ecbcd738235035
gdenn <gdennis91@googlemail.com>
2018-04-18 23:04:44 +0200

    Update cell-reaper pipeline

--- ci/deploy-cf.sh
+++ ci/deploy-cf.sh
@@ -3,7 +3,9 @@
 # Called by CI/CD to update the cf deployment
 # with the latest cell count configured at cf-deployment-config/cf-deployment/config.yml
 #
-bosh deploy -d cf cf-deployment/cf-deployment.yml \
-  -o <SOME FANCY OPS WE NEED> \
-  -o ops/replace_diego_cell_count.yml \
-  -l cf-deployment-config/cf-deployment/config.yml
+# bosh deploy -d cf cf-deployment/cf-deployment.yml \
+#   -o <SOME FANCY OPS WE NEED> \
+#   -o ops/replace_diego_cell_count.yml \
+#   -l cf-deployment-config/cf-deployment/config.yml
+
+echo "scale by $(cat cf-deployment-config/cf-deployment/config.yml)"
