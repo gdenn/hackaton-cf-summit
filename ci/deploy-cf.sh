bdc0292c44fc3bb1e20a404e29eb37608db77040
Yannic Remmet <github@meee.de>
2018-04-18 14:48:02 -0400

    add cf deployment

--- ci/deploy-cf.sh
+++ ci/deploy-cf.sh
@@ -4,6 +4,8 @@
 # with the latest cell count configured at cf-deployment-config/cf-deployment/config.yml
 #
 bosh deploy -d cf cf-deployment/cf-deployment.yml \
-  -o <SOME FANCY OPS WE NEED> \
+  -o ops/scale-to-one-az.yml \
   -o ops/replace_diego_cell_count.yml \
-  -l cf-deployment-config/cf-deployment/config.yml
+  -o ops/rename-network.yml
+  -l cf-deployment-config/cf-deployment/config.yml \
+  -v network_name=default
